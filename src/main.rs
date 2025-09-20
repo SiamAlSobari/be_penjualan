c23b9649d929f18912123ddcfd5b75ad94a52fb5
YammD123 <siamqueen52@gmail.com>
2025-09-20 21:26:47 +0700

    feat: setup actix web dan setup sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+use sqlx::mysql::MySqlPoolOptions;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("")
+        .await
+        .expect("connect");
+    HttpServer::new(move || App::new().app_data(web::Data::new(pool.clone())))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }

35b0bc75c3176e05866d307ac3f9bb2632a218b6
YammD123 <siamqueen52@gmail.com>
2025-09-20 21:44:05 +0700

    chore: menambahkan dependensi dotenv untuk baca file env

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use actix_web::{App, HttpServer, web};
+use dotenvy::dotenv;
 use sqlx::mysql::MySqlPoolOptions;
+use std::env;
 
+//main fn actix
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    //ambil db url di env
+    let db_url = env::var("DATABASE_URL").expect("Error env");
+
+    //Pool db atau koneksi db
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
-        .connect("")
+        .connect(&db_url)
         .await
         .expect("connect");
+
+    //koneksi server
+    println!("server berjalan pada http://localhost:8080");
     HttpServer::new(move || App::new().app_data(web::Data::new(pool.clone())))
         .bind(("0.0.0.0", 8080))?
         .run()

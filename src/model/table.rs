7e386f058a866b9b71c699fc526b2a218fcbff50
YammD123 <siamqueen52@gmail.com>
2025-09-21 11:57:08 +0700

    chore: menambahkan dependensi chrono untuk date time tipe data feat: menambahkan beberapa atribute di struct User dan Profile

--- src/model/table.rs
+++ src/model/table.rs
@@ -1,17 +1,21 @@
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 
-
-#[derive(Debug,Deserialize,Serialize,FromRow)]
+#[derive(Debug, Deserialize, Serialize, FromRow)]
 pub struct User {
     pub id: String,
     pub email: String,
-    pub password: String
+    pub password: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
 
-#[derive(Debug,Deserialize,Serialize,FromRow)]
+#[derive(Debug, Deserialize, Serialize, FromRow)]
 pub struct Profile {
-    pub username : String,
-    pub user_id : String,
-    pub avatar_url : String
+    pub username: String,
+    pub user_id: String,
+    pub avatar_url: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
